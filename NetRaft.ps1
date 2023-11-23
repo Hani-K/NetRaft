7a80c99420d3f8bbd90f9a06d1730e5443e897a9
Hani K <h4ni12@gmail.com>
2023-11-23 22:49:55 +0100

    UTF8-BOM to UTF8

--- NetRaft.ps1
+++ NetRaft.ps1
@@ -1,4 +1,4 @@
-﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Windows.Forms
 
 $FormObject      = [System.Windows.Forms.Form]
 $LabelObject     = [System.Windows.Forms.Label]
